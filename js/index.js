cd5bf8c328c816e2d877d28279b24bd64824a505
Mario <mariojudoka@gmail.com>
2020-10-26 08:53:24 -0600

    login -falta base de dato

--- js/index.js
+++ js/index.js
@@ -1,35 +1,38 @@
-var btnLogin = document.getElementById("btnLogin");
+//This Open Google Account Authentication method
+var btnLogin = document.getElementById('btnLogin')
+var btnLogout = document.getElementById('btnLogout')
 
-firebase.auth().onAuthStateChanged(function(user){
-    if (user) {
-      console.log(user);
-      //nombre del usuario
-      console.log(user.displayName);
-    }else {
-      console.log("no tenemos usuario");
-    }
-  });
+btnLogin.addEventListener('click', function(){
+    //Handle Funtionallity
+    event.preventDefault()  
+    //Specify that the only permission I want to google sign is user contacts
+    var provider = new firebase.auth.GoogleAuthProvider()
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly')
+    firebase.auth().signInWithPopup(provider)
+    .then(function(userData){
+        console.log(userData)
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+})
+btnLogout.addEventListener('click', function(){
+    //Funcionallity
+    event.preventDefault()  
+    firebase.auth().signOut()
   
-  btnLogin.addEventListener("click", function(){
-    //event.preventDefault();
-    var provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    //defino en que leguaje le va a salir el Popup de autenticacion
-    firebase.auth().languageCode = 'es';
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
-  });
+  
+})
+
+//When a User for any method of Authentication Init or Close sesion, this appear
+firebase.auth().onAuthStateChanged(function(user){
+    if(user){
+        //If User is authenticated
+        btnLogin.style.display = "none"
+        btnLogout.style.display = "block"
+    }else{        
+        btnLogin.style.display = "block"
+        btnLogout.style.display = "none"
+    }
+    
+})
